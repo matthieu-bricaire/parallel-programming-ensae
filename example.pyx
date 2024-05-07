bc8a9c4c3b43d07126f435bb47cf5309e5798728
mbricaire <matthieu.bricaire@ensae.fr>
2024-05-07 19:21:51 +0000

    Deleted useless files

--- example.pyx
+++
@@ -1,5 +0,0 @@
-cdef extern from "my_function.cpp":
-    void my_cpp_function()
-
-def call_cpp_function():
-    my_cpp_function()
--- my_function.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void my_cpp_function() {
-    std::cout << "Hello from C++!" << std::endl;
-}
