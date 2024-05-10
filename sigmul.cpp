935c467f22c1370d153d6af2d596687a360aa3ee
mbricaire <matthieu.bricaire@ensae.fr>
2024-05-10 15:22:04 +0000

    Minor improvement

--- sigmul.cpp
+++ sigmul.cpp
@@ -6,8 +6,6 @@
 #define my_matrix std::vector<std::vector<float>>
 
 my_matrix sigmul(const my_matrix& A) {
-    int n_threads = omp_get_max_threads();
-    std::cout << "Number of threads: " << n_threads << std::endl;
 
     // Declare size of A
     int numRows = A.size();
@@ -17,7 +15,7 @@ my_matrix sigmul(const my_matrix& A) {
     my_matrix B(numRows, std::vector<float>(numCols));
 
     // Perform the operation in parallel
-    #pragma omp parallel for
+    #pragma omp parallel for simd collapse(2)
     for (int i = 0; i < numRows; i++) {
         for (int j = 0; j < numCols; j++) {
             float a = A[i][j];
