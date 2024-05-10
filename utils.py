52cb94cc6f1c1c081a5f5e9bc4a50eb523235cb5
mbricaire <matthieu.bricaire@ensae.fr>
2024-05-10 15:52:54 +0000

    Further experiments

--- utils.py
+++ utils.py
@@ -7,9 +7,9 @@ import numpy as np
 torch.manual_seed(29)
 
 
-def sigmul_from_tensor_cpp(A):
-    Al = A.tolist()
-    B = compute_sigmul_cpp(Al)
+def sigmul_from_tensor_cython(A):
+    An = np.asarray(A, dtype=np.float32)
+    B = compute_sigmul_cython(An)
     return torch.tensor(B)
 
 
@@ -26,7 +26,7 @@ def compute_time(A, sigmul_fun, n_rep):
     return (time.time() - start) / n_rep
 
 
-def compare_times(A, n_rep=50, include_cpp=True):
+def compare_times(A, n_rep=50, include_cpp=True, include_conv_cython=False):
     times = {}    
 
     if include_cpp:
@@ -34,12 +34,12 @@ def compare_times(A, n_rep=50, include_cpp=True):
         Al = A.tolist()
         times['sigmul_cpp'] = compute_time(Al, compute_sigmul_cpp, n_rep)
 
+    if include_conv_cython:
+        times['sigmul_cython_with_conv'] = compute_time(A, sigmul_from_tensor_cython, n_rep)
+
     # Full cython version
     An = np.asarray(A, dtype=np.float32)
     times['sigmul_cython'] = compute_time(An, compute_sigmul_cython, n_rep)
-
-    # # With sigmul from tensor - convert to list and back to tensor
-    # times['tensor_2'] = compute_time(A, sigmul_from_tensor, n_rep)
 
     # Torch based version
     times['sigmul_torch'] = compute_time(A, sigmul_torch, n_rep)
