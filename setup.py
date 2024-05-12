a2fc7bb55871bf9a99cdcf634436cf6bcee1a886
mbricaire <matthieu.bricaire@ensae.fr>
2024-05-12 13:12:33 +0000

    Improved reproducibility

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from distutils.core import setup
 from distutils.extension import Extension
+from distutils.core import setup
 from Cython.Build import cythonize
+import os
+
+include_dir = os.environ.get('CPLUS_INCLUDE_PATH', '/usr/include/c++/11/')
 
 extensions = [
     Extension("sigmulib",
               sources=["interface.pyx"],
-              include_dirs=["/usr/include/c++/11/"],
+              include_dirs=[include_dir],
               extra_compile_args=["-fopenmp"],
               extra_link_args=["-fopenmp"],
               language="c++")
